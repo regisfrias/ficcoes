2a1fc90e8f98ac4618e07649cac183eeea2d1293
Régis Frias <regis.frias@motleyagency.com>
2022-07-15 20:49:01 +0300

    Make sure there is padding for any width.

--- layouts/main.tsx
+++ layouts/main.tsx
@@ -8,7 +8,7 @@ const Wrapper = styled.div`
   margin: auto;
   max-width: calc(100% - ${SPACINGS.padding_sm}px);
   @media screen and (min-width: ${BREAKPOINTS.sm}px) {
-    max-width: ${BREAKPOINTS.sm}px;
+    max-width: ${BREAKPOINTS.sm - SPACINGS.padding_sm}px;
   }
 `
 
