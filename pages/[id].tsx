ff679ea04cfc9b8f50a61317069d629526c31dd4
Régis Frias <regis.frias@motleyagency.com>
2022-07-18 12:50:05 +0300

    Add mandatory props.

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -6,13 +6,17 @@ import { Chapters, PostData } from '../types'
 
 export default function Post({
   chapters,
-  postData
+  postData,
+  toggleTheme,
+  theme,
 }: {
   chapters: Chapters,
-  postData: PostData
+  postData: PostData,
+  toggleTheme: Function,
+  theme: string,
 }) {
   return (
-    <Layout chapters={chapters} title={postData.title}>
+    <Layout chapters={chapters} title={postData.title} toggleTheme={toggleTheme} theme={theme}>
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
