dc64601a680a8767698181ded31ebd65da3f7ba0
Régis Frias <regis.frias@motleyagency.com>
2022-07-23 15:12:16 +0300

    Make cover a separate component and add intro paragraph.

---
+++ components/Cover.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link'
+import styled from 'styled-components'
+import { SPACINGS } from '../constants'
+
+const Wrapper = styled.div`
+  text-align: center;
+`
+
+const P = styled.p`
+  max-width: 300px;
+  margin: auto;
+  margin-top: 10vh;
+`
+
+export default function Cover({lang}: {lang: string}) {
+  return(
+    <Wrapper>
+      <Link href="https://regisfrias.com"><a><em>Régis Frias</em></a></Link><br /><em>2022</em>
+      <P>{ lang === 'pt' ?
+        'Uma série de contos ligeiramente inspirada em (ou, mais precisamente, surrupiada de) J. L. Borges.'
+      :
+        'A series of short stories slightly inspired by (or more precisely stolen from) J. L. Borges.'
+      }</P>
+    </Wrapper>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,9 @@
 import Link from 'next/link'
-import styled from 'styled-components'
 import { getSortedPostsData } from '../lib/chapters'
 import Layout from '../layouts/main'
+import Cover from '../components/Cover'
 import { Chapters, PostData } from '../types'
 
-const Cover = styled.div`
-  text-align: center;
-`
 
 function Fictions({
   chapters,
@@ -25,8 +22,16 @@ function Fictions({
   setFontSize: Function,
 }) {
   return (
-    <Layout chapters={chapters} title='Ficções' toggleTheme={toggleTheme} toggleLanguage={toggleLanguage} theme={theme} lang={lang} setFontSize={setFontSize}>
-      <Cover><Link href="https://regisfrias.com"><a><em>Régis Frias</em></a></Link><br /><em>2022</em></Cover>
+    <Layout
+      chapters={chapters}
+      title='Ficções'
+      toggleTheme={toggleTheme}
+      toggleLanguage={toggleLanguage}
+      theme={theme}
+      lang={lang}
+      setFontSize={setFontSize}
+    >
+      <Cover lang={lang}/>
     </Layout>
   )
 }
